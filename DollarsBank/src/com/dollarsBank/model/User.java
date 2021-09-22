b177b425fd81425b7c4842e6ff96008703ca368e
Pthomas2 <pthomas1@lions.molloy.edu>
2021-09-22 16:11:37 -0400

    Create User.java

---
+++ DollarsBank/src/com/dollarsBank/model/User.java
@@ -0,0 +1,119 @@
+package com.dollarsBank.model;
+
+
+public class User {
+	
+
+	private String username;
+	
+	private Long mbrId;
+	
+	private String fname;
+
+	private String lname;
+
+	private String email;
+
+	private String password;
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+
+	public Long getMbrId() {
+		return mbrId;
+	}
+
+
+
+
+	public void setMbrId(Long mbrId) {
+		this.mbrId = mbrId;
+	}
+
+
+
+
+	public String getFname() {
+		return fname;
+	}
+
+
+
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+
+
+
+	public String getLname() {
+		return lname;
+	}
+
+
+
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "{" +
+			" mbrId='" + getMbrId() + "'" +
+			", fname='" + getFname() + "'" +
+			", lname='" + getLname() + "'" +
+			", email='" + getEmail() + "'" +
+			", username='" + getUsername() + "'" +
+			", password='" + getPassword() + "'" +
+			"}";
+	}
+
+	
+	
+}
