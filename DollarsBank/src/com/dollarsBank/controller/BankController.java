af3fb2d737a14b80e3b30dbade7d9ce2ba53c538
Pthomas2 <pthomas1@lions.molloy.edu>
2021-09-23 12:34:05 -0400

    Create BankController.java

---
+++ DollarsBank/src/com/dollarsBank/controller/BankController.java
@@ -0,0 +1,13 @@
+package com.dollarsBank.controller;
+
+import java.util.List;
+
+import com.dollarsBank.model.User;
+
+public class BankController {
+	
+	public void login(String username, String password, List<User> currentUsers) {
+		if()
+	}
+
+}
