7eb589c67829567e4ed9ea7ecd6f8cf507c91d31
Pthomas2 <pthomas1@lions.molloy.edu>
2021-09-23 10:38:38 -0400

    Create Account.java

---
+++ DollarsBank/src/com/dollarsBank/model/Account.java
@@ -0,0 +1,51 @@
+package com.dollarsBank.model;
+
+public class Account {
+	
+	private Long acctId;
+	
+	private Long mbrId;
+	
+	private double balance;
+	
+	public Account(Long acctId, Long mbrId, double balance) {
+		super();
+		this.acctId = acctId;
+		this.mbrId = mbrId;
+		this.balance = balance;
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	public Long getAcctId() {
+		return acctId;
+	}
+
+	public void setAcctId(Long acctId) {
+		this.acctId = acctId;
+	}
+
+	public Long getMbrId() {
+		return mbrId;
+	}
+
+	public void setMbrId(Long mbrId) {
+		this.mbrId = mbrId;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	
+
+
+
+}
